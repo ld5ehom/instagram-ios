10404e5155ad9a1fb168065502d97978d137ea62
Taewook Park <ld5ehom@gmail.com>
2024-09-17 23:55:18 -0700

    Init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Messenger iOS App
+
+## Project Overview
+- Instagram app for practicing tab bar
+- Utilized: Swift, UIKit, MVVM
---
+++ instagram-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-ios
+//
+//  Created by TaeWook Park on 9/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
